74ec8e0aa0817fbf0b20ad97dfdc76e47cc2bc36
sushil thakur <sushilt2059@gmail.com>
2025-01-21 08:13:39 +0545

    push

--- views/notes.view.php
+++ views/notes.view.php
@@ -14,7 +14,11 @@
 
   <main>
     <div class="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
-    this is teampage
+   <?php foreach($note as $note): ?>
+      <P><?=$note['body']?></P>
+      <?php endforeach; ?>
+      
+        
     
     </div>
   </main>
