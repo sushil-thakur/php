1c41d0e88792f0e316eb7baaaae992e8d80673f4
sushil thakur <sushilt2059@gmail.com>
2025-01-22 08:38:49 +0545

    push

--- notes.php
+++ notes.php
@@ -1,13 +1,28 @@
 <?php
+
 include 'Database.php';
 include 'utils.php';
-$db =new Database();
-$statement = $db->query("SELECT * FROM note");
-$notes = $statement->fetchAll();
 
-// dd($notes);
+$db = new Database();
 
+// ----Handles note deletion----
+// ----Handles note deletion----
+$method = $_POST['_method'] ?? '';
 
-$navTitle ="Notes";
-include 'views/notes.view.php'
-?>
+if ($method === 'DELETE') {
+  $id = $_POST['id'];
+
+  $db->query("DELETE FROM note WHERE id = :id", [':id' => $id]);
+
+  header('Location: notes.php');
+  exit;
+}
+// ------------------------------
+
+// ----Fetches notes from the database----
+$notes = $db->query("SELECT * FROM note")->fetchAll(PDO::FETCH_ASSOC);
+// ----------------------------------------
+
+$navTitle = "Notes";
+
+include 'views/notes.view.php';
